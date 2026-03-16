182ff6480bc7996cb633b8d6de86533d19fae505
Benjamin Pearce <admin@bcpearce.com>
2026-03-16 02:25:46 -0400

    Added edge case for asteroid collisions

--- src/collisions.rs
+++ src/collisions.rs
@@ -9,7 +9,12 @@ pub fn asteroid_shot_collision(asteroid: &Asteroid, shot: &Shot) -> bool {
 
 /// Determines if a Ship collided with an Asteroid using point-in-polygon test
 pub fn asteroid_ship_collision(asteroid: &Asteroid, ship: &Ship) -> bool {
-    ship.polygon()
+    let ship_polygon = ship.polygon();
+    let asteroid_polygon = &asteroid.polygon();
+    ship_polygon
         .iter()
-        .any(|p| p.in_polygon(&asteroid.polygon()).unwrap())
+        .any(|p| p.in_polygon(asteroid_polygon).unwrap_or(false))
+        || asteroid_polygon
+            .iter()
+            .any(|p| p.in_polygon(&ship_polygon).unwrap_or(false))
 }
